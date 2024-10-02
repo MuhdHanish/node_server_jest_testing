2a02f3533cbd74938a5f1321f2c3164698245b49
MuhdHanish <muhammedhanish11@gmail.com>
2024-10-02 12:54:59 +0530

    refactor: updated app with db dep injection

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
-const app = express();
 
-app.use(express.json());
+function createApp(db) {
+    const app = express();
+    app.use(express.json());
 
-app.post(`/users`, (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        res.status(400).send({ message: "Username and Password is required." });
-    }
-    res.send({ id: "1" });
-});
+    app.post(`/users`, (req, res) => {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).send({ message: "Username and Password are required." });
+        }
+        const userExists = db.findUserByUsername(username);
+        if (userExists) {
+            return res.status(409).send({ message: "Username already taken." });  
+        }
+        const newUser = db.addUser({ username, password });
+        res.status(201).send({ id: newUser.id });
+    });
+
+    return app;
+}
 
 module.exports = {
-    app
+    createApp
 };
