32ffaf39121a60b606636d64bafbeef4d3e85416
MuhdHanish <muhammedhanish11@gmail.com>
2024-10-02 12:15:10 +0530

    feat: added the validation for the 400 status check test

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
 
 app.post(`/users`, (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        res.sendStatus(400);
+    }
     res.send({ id: "1"});
 });
 
