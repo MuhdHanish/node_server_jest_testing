1521a51eca241ebbcc226b474f4b2a6a8eeefcf5
MuhdHanish <muhammedhanish11@gmail.com>
2024-10-02 12:14:49 +0530

    test: added the test for 400 status in post user api

--- app.spec.js
+++ app.spec.js
@@ -4,6 +4,11 @@ const request = require("supertest");
 describe('POST /users', () => {
     describe('when the username and password missing', () => {
         // should respond with a status code of 400
+        it('should response a 400 status code', async () => {
+            const response = await request(app).post("/users").send({});
+            expect(response.statusCode).toBe(400);
+        });
+
     });
     describe('given a username and password', () => {
         // should save teh username and password to the database
@@ -14,7 +19,7 @@ describe('POST /users', () => {
                 password: "Jhone@123"
             });
             expect(response.statusCode).toBe(200);
-        })
+        });
         // should specify json in the content type header
         it('should specify json in the content type header', async () => {
             const response = await request(app).post("/users").send({
@@ -22,14 +27,14 @@ describe('POST /users', () => {
                 password: "Jhone@123"
             });
             expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
-        })
+        });
         // should respond with a json object containing user id
         it('should respond with a json object containing user id', async () => {
             const response = await request(app).post("/users").send({
                 username: "Jhon Doe",
                 password: "Jhone@123"
             });
-            expect(response.body.id).toBeDefined( );
-        })
+            expect(response.body.id).toBeDefined();
+        });
     });
 });
