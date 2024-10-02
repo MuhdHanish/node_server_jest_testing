dd2b5f1c1e68c3f51c567631e7fede8966faad6c
MuhdHanish <muhammedhanish11@gmail.com>
2024-10-02 12:17:47 +0530

    feat: added the validation error message passing in response

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.post(`/users`, (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        res.sendStatus(400);
+        res.status(400).send({ message: "Username and Password is required." });
     }
     res.send({ id: "1"});
 });
